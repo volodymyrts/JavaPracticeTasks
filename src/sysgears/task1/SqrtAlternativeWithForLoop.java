5694609f3dedd6752873ab472cb85a82aad58851
VolodymyrTS <20706601+volodymyrts@users.noreply.github.com>
2020-09-15 10:39:59 +0300

    Removed -if- statement.

--- src/sysgears/task1/SqrtAlternativeWithForLoop.java
+++ src/sysgears/task1/SqrtAlternativeWithForLoop.java
@@ -4,17 +4,11 @@ public class SqrtAlternativeWithForLoop {
 
     public static void main(String[] args) {
 
-        int inputNumber = 1;
-        int sqrtNumber = 0;
+        int inputNumber = 5;
+        int sqrtNumber = 1;
 
-        for (int i = 1; i <= inputNumber; i++) {
-            if (i * i == inputNumber) {
-                sqrtNumber = i;
-                break;
-            } else if (i * i > inputNumber) {
-                sqrtNumber = i - 1;
-                break;
-            }
+        for (int i = 2; i * i <= inputNumber; i++) {
+            sqrtNumber = i;
         }
 
         System.out.println("sqrtNumber = " + sqrtNumber);
