a1ddcdff8144faecd1f2094b6ca7b556372e7325
VolodymyrTS <20706601+volodymyrts@users.noreply.github.com>
2020-09-30 23:14:33 +0300

    Added OCA test question

---
+++ src/oca/Q001.java
@@ -0,0 +1,39 @@
+package oca;
+
+// What is the output of the following code?
+
+// A: 120
+//    121
+//    127
+//
+// B: 120
+//
+// C: default
+//    2
+//
+// D: 120
+//    121
+//    127
+//    default
+
+public class Q001 {
+
+    public static void main(String args[]) {
+        byte foo = 120;
+        switch (foo) {
+            default:
+                System.out.println("default");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            case 120:
+                System.out.println("120");
+            case 121:
+                System.out.println("121");
+            case 127:
+                System.out.println("127");
+                break;
+        }
+    }
+}
