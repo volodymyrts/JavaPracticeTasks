4f36f7e5cc00194c0f67c90235845441bed25036
VolodymyrTS <20706601+volodymyrts@users.noreply.github.com>
2020-10-01 15:53:42 +0300

    Added OCA test question Q004

---
+++ src/oca/Q004.java
@@ -0,0 +1,29 @@
+package oca;
+
+// Which of the following statements are true? (Choose all that apply)
+//
+// A: Checked exceptions are intended to be thrown by the JVM and not the programmer.
+// B: Checked exceptions are required to be caught or declared.
+// C: Errors are intended to be thrown by the JVM and not the programmer.
+// D: Runtime exceptions are required to be caught or declared.
+// E: Runtime exceptions are intended to be thrown by the JVM and not the programmer.
+// F: Errors are required to be caught or declared.
+
+public class Q004 {
+}
+
+// Correct answers:
+//
+// B, C.
+//
+//
+//
+//
+// Note:
+// Only checked exceptions are required to be handled or declared.
+//
+// Runtime exceptions are commonly thrown by both the JVM and programmer code.
+//
+// Checked exceptions are usually thrown by programmer code.
+//
+// Errors are intended to be thrown by the JVM.
--- src/questions/object/HashCodeAndEquals.java
+++ src/questions/object/HashCodeAndEquals.java
@@ -2,12 +2,29 @@ package questions.object;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 class Employee {
     int id;
 
     public Employee(int id) {
         this.id = id;
+    }
+
+    // ----------------------------------
+    // Added equals and hashcode methods
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
 
@@ -19,12 +36,14 @@ public class HashCodeAndEquals {
 
         Map<Employee, String> map = new HashMap<Employee, String>();
         map.put(emp1, "Employee");
-        map.put(emp2, "Employee");
+        map.put(emp2, "Employee"); // Duplicated elements were added to HashMap
 
-        System.out.println(map.size()); // What is the size of the map? Answer: 2
-        // Duplicated elements were added to HashMap
+        System.out.println(map.size()); // What is the size of the map?
+                                        // Answer: 2 (when Employee class has no equals and hashcode methods)
 
-        // -----------------------
+        System.out.println(map.entrySet());
+
+        // ------------------------------
 
         Integer i1 = new Integer(1);
         Integer i2 = new Integer(1);
