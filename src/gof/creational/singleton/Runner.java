845f0578469a15e5b10d3e4bb98294a2212d5d78
VolodymyrTS <20706601+volodymyrts@users.noreply.github.com>
2020-11-07 21:13:00 +0200

    Left only one runner.

--- src/gof/creational/singleton/Runner.java
+++ src/gof/creational/singleton/Runner.java
@@ -4,11 +4,26 @@ public class Runner {
 
     public static void main(String[] args) {
 
-        LazyInitializationSingleton instance1 = LazyInitializationSingleton.getInstance();
-        LazyInitializationSingleton instance2 = LazyInitializationSingleton.getInstance();
+        EagerInitializedSingleton instance1 = EagerInitializedSingleton.getInstance();
+        EagerInitializedSingleton instance2 = EagerInitializedSingleton.getInstance();
 
         System.out.println(instance1 == instance2);       // true
         System.out.println(instance1.equals(instance2));  // true
+        System.out.println();
+
+        StaticBlockInitializationSingleton instance3 = StaticBlockInitializationSingleton.getInstance();
+        StaticBlockInitializationSingleton instance4 = StaticBlockInitializationSingleton.getInstance();
+
+        System.out.println(instance3 == instance4);       // true
+        System.out.println(instance3.equals(instance4));  // true
+        System.out.println();
+
+        LazyInitializationSingleton instance5 = LazyInitializationSingleton.getInstance();
+        LazyInitializationSingleton instance6 = LazyInitializationSingleton.getInstance();
+
+        System.out.println(instance5 == instance6);       // true
+        System.out.println(instance5.equals(instance6));  // true
+        System.out.println();
 
     }
 
